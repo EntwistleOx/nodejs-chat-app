33a32f800a686a51bbd8bdee208a6c6f7b106f01
Juan Díaz <juan.diaz.12069@gmail.com>
2020-01-21 15:44:41 -0300

    setup socket.io

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const http = require('http')
+const socketio = require('socket.io')
 const path = require('path')
 const port = process.env.PORT || 3000
 
 //Create APP
 const app = express()
+
+const server = http.createServer(app)
+const io = socketio(server)
 
 //Paths
 const publicPath = path.join(__dirname, '../public')
@@ -11,7 +16,12 @@ const publicPath = path.join(__dirname, '../public')
 //Middleware
 app.use(express.static(publicPath))
 
+//Connection event 
+io.on('connection', () => {
+    console.log('New websocket connection!')
+})
+
 //Listening
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is up and running on port ${port}!`)
 })
