8d82b4aa52c06dfc9c115c90d56a726bc6e96644
Juan Díaz <juan.diaz.12069@gmail.com>
2020-01-21 15:14:16 -0300

    add comments

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ const port = process.env.PORT || 3000
 //Paths
 const publicPath = path.join(__dirname, '../public')
 
+//Middleware
 app.use(express.static(publicPath))
 
+//Listening
 app.listen(port, () => {
     console.log(`Server is up and running on port ${port}`)
 })
