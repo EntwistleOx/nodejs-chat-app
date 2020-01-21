4558747aedecec9ff1febb361c1b174350c01a0a
Juan Díaz <juan.diaz.12069@gmail.com>
2020-01-21 15:11:36 -0300

    setup express, serve up public dir

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
-const path = require('path')
 const app = express()
-const port = process.env.PORT | 3000
+const path = require('path')
+const port = process.env.PORT || 3000
 
-app.use(express.json())
+//Paths
+const publicPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicPath))
+
 app.listen(port, () => {
     console.log(`Server is up and running on port ${port}`)
 })
