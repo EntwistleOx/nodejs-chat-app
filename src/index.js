739776014ffc74b735685e7a4a5bb0d5b8848805
Juan Díaz <juan.diaz.12069@gmail.com>
2020-01-21 15:33:46 -0300

    add socket.io

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
-const app = express()
 const path = require('path')
 const port = process.env.PORT || 3000
+
+//Create APP
+const app = express()
 
 //Paths
 const publicPath = path.join(__dirname, '../public')
@@ -11,5 +13,5 @@ app.use(express.static(publicPath))
 
 //Listening
 app.listen(port, () => {
-    console.log(`Server is up and running on port ${port}`)
+    console.log(`Server is up and running on port ${port}!`)
 })
