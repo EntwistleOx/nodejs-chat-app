e59f48d0e9e84f6ba9c1164510f0ab5a14acccd6
Juan Díaz <juan.diaz.12069@gmail.com>
2020-01-22 12:05:08 -0300

    Geolocation Browser API

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,6 @@
 const socket = io()
 const form = document.querySelector('#message-form')
+const btnLocation = document.querySelector('#send-location')
 // const msgTxt = document.querySelector('input')
 
 // socket.on('countUpdated', (count) => {
@@ -19,4 +20,17 @@ form.addEventListener('submit', (e) => {
     e.preventDefault()
     const message = e.target.elements.message.value
     socket.emit('sendMessage', message)
+})
+
+btnLocation.addEventListener('click', () => {
+    if(!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser.')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    })
 })
