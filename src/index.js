cd964f8f0b696fab72068b3bac2be8af99003f8a
Juan Díaz Ortega <juan.diaz.12069@gmail.com>
2020-01-25 22:15:17 -0300

    socket.io rooms

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+/**
+* socket.emit -> send events to specific client
+* io.emit -> send events to every connected client
+* socket.broadcast.emit -> send events to every connected client except this one
+* io.to.emit -> send events to every connected client in a specific room
+* socket.broadcast.to.emit -> send events to every connected client except this one, limited to a specific room
+*/
 const express = require('express')
 const http = require('http')
 const socketio = require('socket.io')
@@ -18,30 +25,28 @@ const publicPath = path.join(__dirname, '../public')
 //Middleware
 app.use(express.static(publicPath))
 
-// let count = 0
-
 //Connection event 
 //param socket is an object and contains info about new connections
 io.on('connection', (socket) => {
     console.log('New websocket connection!')
-    socket.emit('message', generateMessage('Welcome!'))
 
-    //broadcast. -> emmits to all connections but this particular socket
-    socket.broadcast.emit('message', generateMessage('A new user has joined!'))
+    socket.on('join', ({ username, room }) => {
+        socket.join(room)
+        socket.emit('message', generateMessage('Welcome!')) 
+        socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined!`))
+    })
 
-    //socket. -> emmits only to 1 connection
     socket.on('sendMessage', (message, callback) => {
         //Bad Words
         const filter = new Filter()
-
         if (filter.isProfane(message)) {
             return callback('Profanity is not allowed!')
         }
-
-        //io. -> emmits the event to all connections
-        io.emit('message', generateMessage(message))
+        io.to('Santiago').emit('message', generateMessage(message))
         callback()
     })
+
+
 
     socket.on('sendLocation', (coords, callback) => {
         io.emit(
@@ -61,3 +66,4 @@ io.on('connection', (socket) => {
 server.listen(port, () => {
     console.log(`Server is up and running on port ${port}!`)
 })
+
